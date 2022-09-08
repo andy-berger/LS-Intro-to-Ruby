093a0cce04495477695c81578d676e9363d5e1cf
Andy Berger <andreas.berger@webinit.net>
2022-09-08 18:40:57 +0200

    Adding my solution to exercise 2

---
+++ 1 - Intro to programming/The Basics/2.rb
@@ -0,0 +1,4 @@
+puts (1234 % 10000).to_s[0] # finds thousands place
+puts (1234 % 1000).to_s[0]  # finds hundreds place
+puts (1234 % 100).to_s[0]   # finds tens place
+puts 1234 % 10              # finds ones place
